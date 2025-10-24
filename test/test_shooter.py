fcf8ee8b60e146ce41752d441391672b75f571bf
Axel Kohlmeyer <akohlmey@gmail.com>
2025-10-23 23:04:43 -0400

    speed up test by reducing sleep time

--- test/test_shooter.py
+++ test/test_shooter.py
@@ -8,14 +8,14 @@ import time
 subprocess.check_output("rm -f shot.png", shell=True, stderr=subprocess.STDOUT)
 vfb = subprocess.Popen(["Xvfb", ":1", "-screen", "0", "1024x768x24"])
 os.environ["DISPLAY"] = ":1.0"
-time.sleep(2)
+time.sleep(0.5)
 
 if (len(sys.argv) < 2):
     vfb.terminate()
     sys.exit("Error: missing command line arguments")
 
 subprocess.check_output(format("%s shot.png" % sys.argv[1]), shell=True, stderr=subprocess.STDOUT)
-time.sleep(1)
+time.sleep(0.5)
 vfb.terminate()
 
 if not os.path.exists("shot.png"):
