dc6066f83a6ca3331a8e1323f76b9e5be3ca13bf
Axel Kohlmeyer <akohlmey@gmail.com>
2025-10-23 22:49:32 -0400

    wrap screen shot test with python script launching Xvfb

---
+++ test/test_shooter.py
@@ -0,0 +1,23 @@
+# -*- Python -*-
+
+import os, sys
+import subprocess
+import time
+
+# launch virtual frame buffer
+subprocess.check_output("rm -f shot.png", shell=True, stderr=subprocess.STDOUT)
+vfb = subprocess.Popen(["Xvfb", ":1", "-screen", "0", "1024x768x24"])
+os.environ["DISPLAY"] = ":1.0"
+time.sleep(2)
+
+if (len(sys.argv) < 2):
+    vfb.terminate()
+    sys.exit("Error: missing command line arguments")
+
+subprocess.check_output(format("%s shot.png" % sys.argv[1]), shell=True, stderr=subprocess.STDOUT)
+time.sleep(1)
+vfb.terminate()
+
+if not os.path.exists("shot.png"):
+    sys.exit("Error: screenshot not created")
+os.remove("shot.png")
