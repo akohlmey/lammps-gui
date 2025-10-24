f473401c77b5dd08f9c175505fdab39d2c6d5131
Axel Kohlmeyer <akohlmey@gmail.com>
2025-10-24 13:31:23 -0400

    convert test_shooter.py to use unittest and Pillow

--- test/test_shooter.py
+++ test/test_shooter.py
@@ -1,12 +1,42 @@
 # -*- Python -*-
 
-import os, sys
+import os
 import subprocess
-import time
+import pyautogui
+import unittest
+from PIL import Image
+pyautogui.PAUSE = 0.2
 
-subprocess.check_output("shooter shot.png", shell=True, stderr=subprocess.STDOUT)
-time.sleep(0.2)
+class ScreenshotChecks(unittest.TestCase):
+    """Tests for the 'shooter' screenshot wrapper"""
 
-if not os.path.exists("shot.png"):
-    sys.exit("Error: screenshot not created")
-os.remove("shot.png")
+    def setUp(self):
+        """Clean up leftover files from previous runs"""
+        if os.path.exists("shot.png"):
+            os.remove("shot.png")
+
+    def tearDown(self):
+        """Clean up after test completion"""
+        if os.path.exists("shot.png"):
+            os.remove("shot.png")
+
+    def testCreateImage(self):
+        """Test if 'shooter' can run and if it will create an all-black PNG image file"""
+        subprocess.check_output("shooter shot.png", shell=True, stderr=subprocess.STDOUT)
+        width, height = pyautogui.size()
+        self.assertTrue(os.path.exists("shot.png"))
+        with Image.open("shot.png") as im:
+            self.assertEqual(im.format, "PNG")
+            self.assertEqual(im.size, (width,height))
+
+            im = im.convert("RGB")
+            px = im.load()
+            # check format, size, and that it is all black
+            self.assertEqual(px[100,100], (0,0,0))
+            self.assertEqual(px[100,500], (0,0,0))
+            self.assertEqual(px[500,100], (0,0,0))
+            self.assertEqual(px[500,500], (0,0,0))
+
+##############################
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
