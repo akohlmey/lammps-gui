c95279aa5f760acb92b02328df7984ee9a5485fa
Axel Kohlmeyer <akohlmey@gmail.com>
2025-10-24 06:39:00 -0400

    use xvfb-run to launch GUI tests

--- test/test_shooter.py
+++ test/test_shooter.py
@@ -4,19 +4,8 @@ import os, sys
 import subprocess
 import time
 
-# launch virtual frame buffer
-subprocess.check_output("rm -f shot.png", shell=True, stderr=subprocess.STDOUT)
-vfb = subprocess.Popen(["Xvfb", ":1", "-screen", "0", "1024x768x24"])
-os.environ["DISPLAY"] = ":1.0"
-time.sleep(0.5)
-
-if (len(sys.argv) < 2):
-    vfb.terminate()
-    sys.exit("Error: missing command line arguments")
-
-subprocess.check_output(format("%s shot.png" % sys.argv[1]), shell=True, stderr=subprocess.STDOUT)
-time.sleep(0.5)
-vfb.terminate()
+subprocess.check_output("shooter shot.png", shell=True, stderr=subprocess.STDOUT)
+time.sleep(0.2)
 
 if not os.path.exists("shot.png"):
     sys.exit("Error: screenshot not created")
