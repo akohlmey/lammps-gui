1506bfebd03f1ef615d386624776a107734fff9a
Axel Kohlmeyer <akohlmey@gmail.com>
2025-11-01 17:51:45 -0400

    reverse special case logic for improved source code readability

--- src/main.cpp
+++ src/main.cpp
@@ -35,12 +35,13 @@ int main(int argc, char *argv[])
     qRegisterMetaTypeStreamOperators<QList<QString>>("QList<QString>");
 #endif
 
-#ifndef Q_OS_MACOS
+#if defined(Q_OS_MACOS)
+    // macOS does not support the "C" locale with UTF-8 encoding,
+    // Since Qt requires UTF-8 we use "en_US" instead.
+    qputenv("LC_ALL", "en_US.UTF-8");
+#else
     // enforce using the plain ASCII C locale with UTF-8 encoding within the GUI.
     qputenv("LC_ALL", "C.UTF-8");
-#else
-    // macOS does not support "C" locale with UTF-8 encoding, but Qt requires UTF-8
-    qputenv("LC_ALL", "en_US.UTF-8");
 #endif
 
     // disable processor affinity for threads by default
